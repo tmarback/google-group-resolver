c4a6d1091eb61d71f8de2a1fd47ca04554866cb8
Thiago Marback <tmarback@sympho.dev>
2024-07-10 23:27:33 -0700

    Add Spring application configuration

--- app/src/main/java/dev/sympho/google_group_resolver/Application.java
+++ app/src/main/java/dev/sympho/google_group_resolver/Application.java
@@ -1,13 +1,20 @@
 package dev.sympho.google_group_resolver;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @SuppressWarnings( "required.method.not.called" ) // registerShutdownHook deals with close()
+    public static void main( String[] args ) {
+
+        new SpringApplicationBuilder( Application.class )
+                .web( WebApplicationType.REACTIVE )
+                .run( args )
+                .registerShutdownHook();
+
+    }
 
 }
