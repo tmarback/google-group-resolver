90a5cd1c6838c384734940be2e18c4b6a8f79abe
Thiago Marback <tmarback@sympho.dev>
2024-07-22 02:30:53 -0700

    Add missing dependency locks

--- buildSrc/src/main/kotlin/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-conventions.gradle.kts
@@ -25,6 +25,19 @@ dependencyLocking {
     lockAllConfigurations()
 }
 
+tasks.register("resolveAndLockAll") {
+    notCompatibleWithConfigurationCache("Filters configurations at execution time")
+    doFirst {
+        require(gradle.startParameter.isWriteDependencyLocks) { "$path must be run from the command line with the `--write-locks` flag" }
+    }
+    doLast {
+        configurations.filter {
+            // Add any custom filtering on the configurations to be resolved
+            it.isCanBeResolved && it.getName() != "checkerFrameworkAnnotatedJDK"
+        }.forEach { it.resolve() }
+    }
+}
+
 testing {
     suites { 
         withType(JvmTestSuite::class).configureEach { 
