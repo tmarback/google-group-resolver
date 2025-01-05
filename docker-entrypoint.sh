47ac78c249f8ade6baa0ca3e9e1f0ec1c8f066a6
Thiago Marback <tmarback@sympho.dev>
2025-01-05 01:35:46 +0000

    Enable virtual threads

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,9 @@
 
 set -e
 
-exec java -XX:+UseContainerSupport $JVM_ARGS $EXTRA_JVM_ARGS -server -jar /opt/app/application.jar "$@"
+exec java -XX:+UseContainerSupport \
+    $JVM_ARGS $EXTRA_JVM_ARGS \
+    -Dreactor.schedulers.defaultBoundedElasticOnVirtualThreads=true \
+    -server \
+    -jar /opt/app/application.jar \
+    "$@"
